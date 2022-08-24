5f10ae0a71f806f4290f8c049eb6da13b78d7987
Nikolas <nikolas.garza5@gmail.com>
2022-08-24 02:36:10 -0700

    non-numerical operators

--- practice.sql
+++ practice.sql
@@ -68,3 +68,50 @@ FROM orders
 WHERE total_amt_usd < 500
 ORDER BY total_amt_usd DESC
 LIMIT 10;
+
+
+-- Filter the accounts table to include the company name, website, and the primary point of contact (primary_poc) just for
+-- the Exxon Mobil company in the accounts table.
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name LIKE 'Exxon Mobil';
+
+
+-- Create a column that divides the standard_amt_usd by the standard_qty to find the unit price for standard paper for each order. \
+-- Limit the results to the first 10 orders, and include the id and account_id fields.
+SELECT  id,
+        account_id,
+        (standard_amt_usd / standard_qty) AS standard_unit_price
+FROM orders
+LIMIT 10;
+
+
+-- Write a query that finds the percentage of revenue that comes from poster paper for each order. You will need to use only the columns that end
+-- with _usd. (Try to do this without using the total column.) Display the id and account_id fields also. NOTE - you will receive an error with the
+-- correct solution to this question. This occurs because at least one of the values in the data creates a division by zero in your formula.
+-- You will learn later in the course how to fully handle this issue. For now, you can just limit your calculations to the first 10 orders,
+-- as we did in question #1, and you'll avoid that set of data that causes the problem.
+SELECT  id,
+        account_id,
+        poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd) AS post_revenue_perc
+FROM orders
+LIMIT 10;
+
+
+-- All the companies whose names start with 'C'.
+SELECT  name
+FROM accounts
+WHERE name LIKE 'C%'
+LIMIT 10;
+
+-- All companies whose names contain the string 'one' somewhere in the name.
+SELECT  name
+FROM accounts
+WHERE name LIKE '&one%'
+LIMIT 10;
+
+-- All companies whose names end with 's'.
+SELECT  name
+FROM accounts
+WHERE name LIKE '%s'
+LIMIT 10;
