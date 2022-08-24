9f24909ae486d62ea6c8ebdcc35144f9cc542e2a
Nikolas <nikolas.garza5@gmail.com>
2022-08-24 02:08:04 -0700

    solved 2 order by problems

--- practice.sql
+++ practice.sql
@@ -58,6 +58,13 @@ LIMIT 10;
 SELECT *
 FROM orders
 WHERE gloss_amt_usd >= 1000
+ORDER BY gloss_amt_usd ASC
+LIMIT 5;
 
 -- Pulls the first 10 rows and all columns from the orders table that have a total_amt_usd
 -- less than 500.
+SELECT *
+FROM orders
+WHERE total_amt_usd < 500
+ORDER BY total_amt_usd DESC
+LIMIT 10;
