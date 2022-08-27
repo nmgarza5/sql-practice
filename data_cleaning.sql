e49ce476eee7ee1d7654fb944277c33a45da1ed4
Nikolas <nikolas.garza5@gmail.com>
2022-08-26 17:27:48 -0700

    CONACT complete

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -51,4 +51,71 @@ SELECT
     type,
     CONCAT(LEFT(CAST((count / (SELECT SUM(count) FROM vowel_counts) * 100) as varchar),5), '%') as proportion
 FROM vowel_counts
-GROUP BY 1, count
+GROUP BY 1, count;
+
+
+
+
+-- Use the accounts table to create first and last name columns that hold the first and last names for the primary_poc.
+
+
+ SELECT
+    LEFT(primary_poc, POSITION(' ' in primary_poc)-1) as first_name,
+    RIGHT(primary_poc, (LENGTH(primary_poc) - POSITION(' ' in primary_poc))) as last_name
+FROM accounts;
+
+-- Now see if you can do the same thing for every rep name in the sales_reps table. Again provide first and last
+-- name columns.
+ SELECT
+    LEFT(name, POSITION(' ' in name)-1) as first_name,
+    RIGHT(name, (LENGTH(name) - POSITION(' ' in name))) as last_name
+FROM sales_reps;
+
+
+
+-- Each company in the accounts table wants to create an email address for each primary_poc. The email address should be
+-- the first name of the primary_poc . last name primary_poc @ company name .com.
+WITH email_components as
+                        (SELECT
+                            name as company_name,
+                            primary_poc,
+                            LEFT(primary_poc, POSITION(' ' in primary_poc)-1) as first_name,
+                            RIGHT(primary_poc, (LENGTH(primary_poc) - POSITION(' ' in primary_poc))) as last_name,
+                            REPLACE(name, ' ', '') as name
+                        FROM accounts
+                            )
+SELECT
+    company_name,
+    primary_poc,
+    CONCAT(first_name, '.', last_name, '@', name, '.com') as email_address
+FROM email_components;
+
+
+-- You may have noticed that in the previous solution some of the company names include spaces, which will certainly not
+-- work in an email address. See if you can create an email address that will work by removing all of the spaces in the
+-- account name, but otherwise your solution should be just as in question 1. Some helpful documentation is here.
+
+
+-- We would also like to create an initial password, which they will change after their first log in. The first password
+-- will be the first letter of the primary_poc's first name (lowercase), then the last letter of their first name
+-- (lowercase), the first letter of their last name (lowercase), the last letter of their last name (lowercase), the
+-- number of letters in their first name, the number of letters in their last name, and then the name of the company
+-- they are working with, all capitalized with no spaces.
+WITH password_components as
+        (SELECT
+            primary_poc,
+            LEFT(LOWER(primary_poc), 1) as first_first,
+            RIGHT(LEFT(LOWER(primary_poc), POSITION(' ' in primary_poc)-1), 1) as first_last,
+            LEFT(RIGHT(LOWER(primary_poc), (LENGTH(primary_poc) - POSITION(' ' in primary_poc))), 1) as last_first,
+            RIGHT(RIGHT(LOWER(primary_poc), (LENGTH(primary_poc) - POSITION(' ' in primary_poc))), 1) as last_last,
+            LENGTH(LEFT(primary_poc, POSITION(' ' in primary_poc)-1)) as first_name_length,
+            LENGTH(RIGHT(primary_poc, (LENGTH(primary_poc) - POSITION(' ' in primary_poc)))) as last_name_length,
+            REPLACE(UPPER(name), ' ', '') as company_name
+        FROM accounts)
+
+SELECT
+    company_name,
+    primary_poc,
+    CONCAT(first_first, first_last, last_first, last_last, first_name_length,
+            last_name_length, company_name) as password
+FROM password_components;
